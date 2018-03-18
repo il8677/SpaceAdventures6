414d1198ffc0055118a7128fc726a05ad14a5093
il8677 <is48le50@isdedu.de>
2018-03-18 19:39:36 +0100

    Added health, added movementl added action menu

--- actor.cpp
+++ actor.cpp
@@ -5,7 +5,42 @@ public:
     string name;
     int x;
     int y;
+    int health;
 
+    void die(){
+
+    }
+    void damage(int amount){
+        health-=amount;
+        if(health <= 0){
+            die();
+        }
+    }
+
+    void left(){
+        x-=1;
+        if(x<0){
+            x=0;
+        }
+    }
+    void right(){
+        x+=1;
+    }
+    void up(){
+        y-=1;
+        if(y<0){y=0;}
+    }
+    void down(){
+        y+=1;
+    }
+    void constrict(int ymax, int xmax){
+        if(y > ymax){
+            y=ymax;
+        }
+        if(x > xmax){
+            x=xmax;
+        }
+    }
     Actor(string n, int xx = 0, int yy = 0){
         name = n;
         x = xx;
--- main.cpp
+++ main.cpp
@@ -4,21 +4,51 @@
 
 using namespace std;
 
-Player player();
+Player player;
+
+#define MAPSIZE 20
 
 Map<> planets[5] = {
-    Map<>(20,20,NULLTILE),
-    Map<>(20,20,NULLTILE),
-    Map<>(20,20,NULLTILE),
-    Map<>(20,20,NULLTILE),
-    Map<>(20,20,NULLTILE),
+    Map<>(MAPSIZE,MAPSIZE,NULLTILE),
+    Map<>(MAPSIZE,MAPSIZE,NULLTILE),
+    Map<>(MAPSIZE,MAPSIZE,NULLTILE),
+    Map<>(MAPSIZE,MAPSIZE,NULLTILE),
+    Map<>(MAPSIZE,MAPSIZE,NULLTILE),
 };
-
+void printTilesOfMap(Map<> * themap){
+    for(int y = 0; y<themap->getx(); y++){
+        for(int x = 0; x<themap->gety(); x++){
+            if(x==player.x && y==player.y){setColor(WHITE); cout << "PP";}else{
+                setColor(themap->getTile(x,y)->getColour());
+                cout<<themap->getTile(x,y)->getStyle();
+            }
+        }cout<<endl;}
+}
 int main(){
     initializeTileTemplates();
-    
-    for(int i = 0; i < 5; i++){
-        planets[5].~Map();
+    for(int i = 0; i<5; i++){
+        generateTerrainOfMap(&planets[i]);
+    }
+
+    printTilesOfMap(&planets[0]);
+
+    while(true){
+        cls();
+        printTilesOfMap(&planets[player.z]);
+        cout <<  planets[player.z].getTile(player.x, player.y)->getFlavour();
+        char action = getch();
+        if(action == 'a'){
+            player.left();
+        }else if (action == 's'){
+            player.down();
+        }else if (action == 'd'){
+            player.right();
+        }else if (action=='w'){
+            player.up();
+        }
+    }
+
+    for(int i = 0; i < 5; i++){//De-init
         planets[i].~Map();
     }
     return 0;
